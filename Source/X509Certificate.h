38a4229d9b914d4daac8d449c5e806a09616de96
Richard McDuffey <richardmc@Richards-Mac-mini.local>
2017-04-26 22:41:20 -0500

    Initial Commit

---
+++ Source/X509Certificate.h
@@ -0,0 +1,19 @@
+//
+//  X509Certificate.h
+//  X509Certificate
+//
+//  Created by Richard McDuffey on 4/26/17.
+//  Copyright © 2017 Richard McDuffey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for X509Certificate.
+FOUNDATION_EXPORT double X509CertificateVersionNumber;
+
+//! Project version string for X509Certificate.
+FOUNDATION_EXPORT const unsigned char X509CertificateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <X509Certificate/PublicHeader.h>
+
+
